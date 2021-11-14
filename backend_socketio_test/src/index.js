a2503df43ac0f06fed43f1a0af56b5b8a22ad620
Fadi Serapian <fadi.serapian@gmail.com>
2021-11-14 15:19:21 -0500

    refactor code

--- backend_socketio_test/src/index.js
+++ backend_socketio_test/src/index.js
@@ -8,22 +8,25 @@ let interval = 0;
 let n = 1;
 const emitCall = () => {
   const calls = [`call-${n}`, `call-${++n}`];
+
   for (const call of calls.reverse()) {
     const msg = {
       ID: call.replace('-', '__'),
       duration: parseInt(call.substr(5)),
     };
+
     io.emit('call', msg);
     console.log('Send', msg);
   }
-  let interval = 2 ** counter * 1000;
+
+  interval = (2 ** counter) * 1000;
   counter++;
   setTimeout(emitCall, interval);
 };
 
 io.on('connection', (socket) => {
   if (!userExists) {
-    setTimeout(emitCall, interval);
+    emitCall();
     userExists = true;
   }
 
